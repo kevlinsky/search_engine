5e200181ab4b15a04d1316ebc2535a29cd122603
Artyom Elin <artyom@harvest-thermal.com>
2022-02-16 16:38:16 +0300

    3rd task inverted index updated

--- task_3/inverted_index.py
+++ task_3/inverted_index.py
@@ -1,32 +1,33 @@
 from task_4.tf_idf import get_paragraphs_dict, get_lemmas_and_tokens
 
 
-def create_inverted_index(page_tokens):
-    all_tokens = list()
+def create_inverted_index(page_lemmas):
+    all_lemmas = list()
 
-    for item in page_tokens.values():
-        all_tokens += item
+    for page_num in range(1, len(page_lemmas.keys()) + 1):
+        for item in page_lemmas[page_num].keys():
+            all_lemmas.append(item)
 
-    unique_tokens = list(set(all_tokens))
-    unique_tokens_dict_str = dict()
-    unique_tokens_dict_int = dict()
+    unique_lemmas = list(set(all_lemmas))
+    unique_lemmas_dict_str = dict()
+    unique_lemmas_dict_int = dict()
 
-    for token in unique_tokens:
-        unique_tokens_dict_int[token] = list()
-        unique_tokens_dict_str[token] = list()
-        for page_num, tokens in page_tokens.items():
-            if token in tokens:
-                unique_tokens_dict_int[token].append(page_num)
-                unique_tokens_dict_str[token].append(str(page_num))
+    for lemma in unique_lemmas:
+        unique_lemmas_dict_int[lemma] = list()
+        unique_lemmas_dict_str[lemma] = list()
+        for page_num in range(1, len(page_lemmas.keys()) + 1):
+            if lemma in page_lemmas[page_num].keys():
+                unique_lemmas_dict_int[lemma].append(page_num)
+                unique_lemmas_dict_str[lemma].append(str(page_num))
                 continue
 
-    return unique_tokens_dict_int, unique_tokens_dict_str
+    return unique_lemmas_dict_int, unique_lemmas_dict_str
 
 
 def save_result_file(inverted_index_dict):
     with open('inverted_index.txt', 'w', encoding='utf-8') as file:
-        for token, pages in inverted_index_dict.items():
-            file.write(token + ': ' + ', '.join(pages) + '\n')
+        for lemma, pages in inverted_index_dict.items():
+            file.write(lemma + ': ' + ', '.join(pages) + '\n')
 
 
 if __name__ == '__main__':
@@ -36,7 +37,7 @@ if __name__ == '__main__':
     lemmas, tokens = get_lemmas_and_tokens(paragraphs_dict)
     print('Lemmas and tokens were formed')
 
-    inverted_index_dict_int, inverted_index_dict_str = create_inverted_index(tokens)
+    inverted_index_dict_int, inverted_index_dict_str = create_inverted_index(lemmas)
     print('Inverted index formed')
 
     save_result_file(inverted_index_dict_str)
